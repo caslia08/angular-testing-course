44874137abea7d6b386f002b549f5ea97108d565
Alia <caslia08@gmail.com>
2022-01-20 07:11:33 +0200

    Section 1 and 2
    Completed and applied the changes pertaining to sections one and two from Angular testing masterclass

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,53 +1,57 @@
-import {CalculatorService} from './calculator.service';
-import {LoggerService} from './logger.service';
-import {TestBed} from '@angular/core/testing';
-
+import { TestBed } from "@angular/core/testing";
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
 
     let calculator: CalculatorService,
-        loggerSpy: any;
+        loggerSpy: any
 
-    beforeEach(()=> {
+    //This block is called before each test
+    beforeEach(() => {
 
-        console.log("Calling beforeEach");
+        //Set-Up phase where we prepare the components of the service we want to test   
 
+        //This is a mock implementation of the dependancy
         loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
 
         TestBed.configureTestingModule({
             providers: [
-                CalculatorService,
+                CalculatorService, 
                 {provide: LoggerService, useValue: loggerSpy}
             ]
-        });
+        }); 
+
 
         calculator = TestBed.get(CalculatorService);
-
     });
 
+    //Describes the methods in the service that we want to test
     it('should add two numbers', () => {
 
-        console.log("add test");
-
+        //Execution phase where we trigger the operation we want to test
         const result = calculator.add(2, 2);
 
-        expect(result).toBe(4);
+        //Series of Test Assertions that will mark the test as a failure or success
+        expect(result).toBe(4, "unexpected addition result");
 
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
     });
 
-
+    //Describes the methods in the service that we want to test
     it('should subtract two numbers', () => {
 
-        console.log("subtract test");
-
+        //Execution phase where we trigger the operation we want to test
         const result = calculator.subtract(2, 2);
 
+        //Series of Test Assertions that will mark the test as a failure or success
         expect(result).toBe(0, "unexpected subtraction result");
 
         expect(loggerSpy.log).toHaveBeenCalledTimes(1);
-
     });
 
-});
+
+
+
+}); 
